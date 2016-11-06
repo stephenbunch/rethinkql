52cb3116159b16b0d0cf356ace9b3e4ce813f8db
Stephen Bunch <bunchclone@gmail.com>
2016-11-06 01:19:09 -0700

    Add type assertion.

--- src/reql/ValueExpression.spec.ts
+++ src/reql/ValueExpression.spec.ts
@@ -1,9 +1,10 @@
 import * as rethinkdb from 'rethinkdb'
 import { expect } from 'chai'
+import ValueExpression from './ValueExpression'
 import * as r from './'
 
 it('should support get', () => {
-  const expr = r.table('test').get('foo')['a']['b']
+  const expr = r.table('test').get('foo')['a']['b'] as ValueExpression
   const query = expr.evaluate({ r: rethinkdb })
   expect(query.toString()).to.equal('r.table("test").get("foo")("a")("b")')
 })
