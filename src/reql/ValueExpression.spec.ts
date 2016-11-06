f2056b7dceedd6ad0dce1618b42911f63d7158d5
Stephen Bunch <bunchclone@gmail.com>
2016-11-06 15:30:51 -0800

    Update test description.

--- src/reql/ValueExpression.spec.ts
+++ src/reql/ValueExpression.spec.ts
@@ -2,7 +2,7 @@ import * as rethinkdb from 'rethinkdb'
 import { expect } from 'chai'
 import * as r from './'
 
-it('should support bracket syntax', () => {
+it('should support get', () => {
   const expr = r.table('test').get('foo').get('a', 'b')
   const query = expr.evaluate({ r: rethinkdb })
   expect(query.toString()).to.equal('r.table("test").get("foo")("a")("b")')
